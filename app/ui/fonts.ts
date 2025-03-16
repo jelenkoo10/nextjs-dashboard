0ce4007970f997eba97f09a46e0f927a6601c1fc
jelenkoo10 <veljkojelenkovic00@gmail.com>
2025-03-16 11:18:56 +0100

    Adding fonts to the project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from "next/font/google";
+
+// Primary font
+export const inter = Inter({ subsets: ["latin"] });
+
+// Secondary font
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
